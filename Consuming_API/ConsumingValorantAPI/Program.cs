6c17405a4c1910c2b3361f0e0ea79bf690758dfc
Fabio Yamashita <yamashitafabio@gmail.com>
2022-08-17 15:37:40 -0300

    Adding Consuming_API

---
+++ Consuming_API/ConsumingValorantAPI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+
+namespace ConsumingValorantAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            getHttpAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task getHttpAsync()
+        {
+            var httpClient = new HttpClient();
+
+            try
+            {
+                var response = await httpClient.GetAsync("https://valorant-api.com/v1/weapons/skinlevels");
+                //var code = response.StatusCode;
+                var message = await response.Content.ReadAsStringAsync();
+                var result = JsonSerializer.Deserialize<SkinLevels>(message);
+
+                Console.WriteLine($"Status = {result.Status}");
+
+                int count = 1;
+                foreach (var skinLevelDetail in result.Data)
+                {
+                    Console.WriteLine($"\n# {count}");
+                    Console.WriteLine($"UUID: {skinLevelDetail.Uuid}");
+                    Console.WriteLine($"Display Name: {skinLevelDetail.DisplayName}");
+                    count++;
+                }
+
+                var reconWeapons = result.Data
+                    .Count(x => !string.IsNullOrEmpty(x.DisplayName) && x.DisplayName.ToUpper().Contains("RECON"));
+                Console.WriteLine($"Total Recon Weapons: {reconWeapons}");
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
---
+++ Consuming_API/ConsumingValorantAPI/SkinLevelsDetails.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsumingValorantAPI
+{
+    public class SkinLevelsDetails : IEquatable<SkinLevelsDetails>
+    {
+        [JsonPropertyName("assetPath")]
+        public string AssetPath { get; set; }
+
+        [JsonPropertyName("displayIcon")]
+        public string DisplayIcon { get; set; }
+
+        [JsonPropertyName("displayName")]
+        public string DisplayName { get; set; }
+
+        [JsonPropertyName("levelItem")]
+        public string LevelItem { get; set; }
+
+        [JsonPropertyName("streamedVideo")]
+        public string StreamedVideo { get; set; }
+
+        [JsonPropertyName("uuid")]
+        public Guid Uuid { get; set; }
+
+
+        public bool Equals(SkinLevelsDetails? other)
+        {
+            return null != other && DisplayName == other.DisplayName;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is SkinLevelsDetails))
+            {
+                return false;
+            }
+
+            SkinLevelsDetails other = obj as SkinLevelsDetails;
+            return DisplayName.Equals(other.DisplayName);
+            //return base.Equals(obj);
+        }
+
+
+        public override int GetHashCode()
+        {
+            return DisplayName.GetHashCode();
+            //return base.GetHashCode();
+        }
+
+    }
+}
