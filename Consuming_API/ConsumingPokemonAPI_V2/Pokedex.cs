3f75a7294c0a6e8c57021437f01ca47c55daf029
Fabio Yamashita <yamashitafabio@gmail.com>
2022-08-21 22:39:01 -0300

    Updating

---
+++ Consuming_API/ConsumingPokemonAPI_V2/Pokedex.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsumingPokemonAPI_V2
+{
+    public class Pokedex
+    {
+        [JsonPropertyName("count")]
+        public long Count { get; set; }
+
+        [JsonPropertyName("next")]
+        public string Next { get; set; }
+
+        [JsonPropertyName("previous")]
+        public string Previous { get; set; }
+
+        [JsonPropertyName("results")]
+        public Pokemon[] Pokemons { get; set; }
+    }
+}
